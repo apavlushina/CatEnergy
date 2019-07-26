6f11117d9dc78dd0250756ee127eadd36f33b0f2
apavlushina <ssemina7@gmail.com>
2019-07-26 22:24:51 +0200

    add gitignore file, project updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var watch = require('gulp-watch');
 
 sass.compiler = require('node-sass');
 
-gulp.task('sass', function () {
+gulp.task('sass-compile', function () {
   return gulp.src('./sass/**/*.scss')
+  .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./css'));
 });
 
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
+
+gulp.task('watch', function() {
+  gulp.watch('./sass/**/*.scss', gulp.series('sass-compile'))
+})
